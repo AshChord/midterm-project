1ab5e87be83050ca46ff685b71edae889d3774f6
Jaehyun Shim <168792571+AshChord@users.noreply.github.com>
2024-11-12 10:34:28 +0900

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.24.3/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jhyn/Desktop/Yonsei/JR/2024-2/응용모바일프로그래밍/Lab/finance_manager"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/jhyn/Desktop/Yonsei/JR/2024-2/응용모바일프로그래밍/Lab/finance_manager/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/jhyn/Desktop/Yonsei/JR/2024-2/응용모바일프로그래밍/Lab/finance_manager/.dart_tool/package_config.json"
